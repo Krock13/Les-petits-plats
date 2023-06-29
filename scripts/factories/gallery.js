efc68a4cbf6b7ce98e72abd1daa2c9fbf862bb9b
krock13 <krock.airdrop@hotmail.com>
2023-06-29 17:15:03 +0200

    Code simplification for gallery.js

--- scripts/factories/gallery.js
+++ scripts/factories/gallery.js
@@ -8,71 +8,39 @@ export function galleryFactory(data) {
 	 * Returns the DOM structure for the recipe card.
 	 * @returns {HTMLElement} The recipe card DOM element.
 	 */
-
 	function getRecipeCardDOM() {
 		const article = document.createElement('article');
 		article.className = 'recipe-article';
 
-		const img = document.createElement('img');
-		img.src = picture;
-		img.alt = name;
-		img.className = 'recipe-image';
-
-		const card = document.createElement('div');
-		card.className = 'recipe-card';
-
-		const h3 = document.createElement('h3');
-		h3.textContent = name;
-		h3.className = 'recipe-title';
-
-		const Entitled1 = document.createElement('h4');
-		Entitled1.textContent = 'recette';
-		Entitled1.className = 'entitled1';
-
-		const step = document.createElement('p');
-		step.innerText = description;
-		step.className = 'recipe-description';
-
-		const Entitled2 = document.createElement('h4');
-		Entitled2.textContent = 'ingrédients';
-		Entitled2.className = 'entitled2';
-
-		const ingredientGrid = document.createElement('div');
-		ingredientGrid.className = 'ingredient-grid';
-
-		ingredients.forEach((ingredient) => {
-			const ingredientDiv = document.createElement('div');
-			ingredientDiv.className = 'ingredient';
-
-			const ingredientName = document.createElement('h5');
-			ingredientName.textContent = ingredient.ingredient;
-
-			const ingredientQuantity = document.createElement('p');
-			if (ingredient.quantity && ingredient.unit) {
-				ingredientQuantity.textContent = `${ingredient.quantity} ${ingredient.unit}`;
-			} else if (ingredient.quantity) {
-				ingredientQuantity.textContent = ingredient.quantity;
-			}
-
-			ingredientDiv.appendChild(ingredientName);
-			ingredientDiv.appendChild(ingredientQuantity);
-			ingredientGrid.appendChild(ingredientDiv);
-		});
-
-		const timeDiv = document.createElement('div');
-		timeDiv.className = 'time';
-		timeDiv.textContent = time + 'min';
-
-		card.appendChild(h3);
-		card.appendChild(Entitled1);
-		card.appendChild(step);
-		card.appendChild(Entitled2);
-		card.appendChild(ingredientGrid);
-		article.appendChild(img);
-		article.appendChild(card);
-		article.appendChild(timeDiv);
+		article.innerHTML = `
+		<img src="${picture}" alt="${name}" class="recipe-image">
+		<div class="recipe-card">
+		  <h3 class="recipe-title">${name}</h3>
+		  <h4 class="entitled1">recette</h4>
+		  <p class="recipe-description">${description}</p>
+		  <h4 class="entitled2">ingrédients</h4>
+		  <div class="ingredient-grid">
+			${ingredients
+				.map(
+					(ingredient) => `
+				<div class="ingredient">
+				  <h5>${ingredient.ingredient}</h5>
+				  <p>${
+						ingredient.quantity && ingredient.unit
+							? `${ingredient.quantity} ${ingredient.unit}`
+							: ingredient.quantity || ''
+					}</p>
+				</div>
+			  `,
+				)
+				.join('')}
+		  </div>
+		</div>
+		<div class="time">${time}min</div>
+	  `;
 
 		return article;
 	}
+
 	return { getRecipeCardDOM };
 }
